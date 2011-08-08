8f4262042c4a14065f12f475b0350de3c6b770cf
Tim Perevezentsev <riffm2005@gmail.com>
2011-08-09 00:41:17 +0400

    `setup.py`

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+version = '0.1'
+
+setup(
+    name='pycamp',
+    version=version,
+    description='Centralized virtualenvs manager and command executor',
+    long_description=open('README.rst').read(),
+    py_modules=['pycamp'],
+    license='MIT',
+    author='Tim Perevezentsev',
+    author_email='riffm2005@gmail.com',
+    url='http://github.com/riffm/pycamp'
+)
+
